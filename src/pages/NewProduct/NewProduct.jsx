d7289bbb89b79ec3cd0c86c8c3b8d2dd122fb224
Myrza Saiyngali <myrzasaiyngali02@gmail.com>
2021-06-30 18:06:13 +0500

    finished /newProduct

--- src/pages/NewProduct/NewProduct.jsx
+++ src/pages/NewProduct/NewProduct.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import './newProduct.css';
+import "./newProduct.css";
 
-export default function NewProduct() {
+const NewProduct = () => {
   return (
     <div className="newProduct">
-      new Product
+      <h1 className="newProductTitle">Create New Product</h1>
+      <form className="newProductForm">
+        <div className="newProductItem">
+          <label className="newProductLabel">Name</label>
+          <input type="text" placeholder="Iphone 12 PRO" />
+        </div>
+        <div className="newProductItem">
+          <label className="newProductLabel">Stock</label>
+          <input type="text" placeholder="12" />
+        </div>
+        <div className="newProductItem">
+          <label className="newProductLabel" >Active</label>
+          <input type="text" placeholder="Yes" />
+        </div>
+        <button className="newProductButton">Create</button>
+      </form>
     </div>
   )
 }
+
+export default NewProduct;
